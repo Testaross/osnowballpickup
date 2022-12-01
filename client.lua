1dcd6ea46b8965bdbd2375060694ba9ecb3bdfc5
Testarossa <austin.w.burrows@gmail.com>
2022-12-01 04:56:24 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,7 +3,7 @@ RegisterKeyMapping('SnowballPickup', 'Pick up snowball', 'keyboard', 'e')
 RegisterCommand('SnowballPickup', function()
     RequestAnimDict('anim@mp_snowball')
     local snowBall = joaat('WEAPON_SNOWBALL')
-    if IsNextWeatherType('XMAS') then
+    if IsNextWeatherType('XMAS') and GetInteriorFromEntity(PlayerPedId()) == 0 then
         local ped = PlayerPedId()-- 
         TaskPlayAnim(ped, 'anim@mp_snowball', 'pickup_snowball', 8.0, -1, -1, 0, 1, 0, 0, 0) -- 
         Wait(1000)
